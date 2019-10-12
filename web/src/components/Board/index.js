fe2875f2bb50ac4c1eedc68842ec9f8d2b52ef43
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-10-12 12:21:02 -0300

    finalizado animações pipefy

--- web/src/components/Board/index.js
+++ web/src/components/Board/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import produce from "immer";
 import { loadLists } from '../../services/api';
+
+import Boardcontext from "./context";
 
 import List from '../List';
 
 import { Container } from './styles';
 
-const list = loadLists();
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(fromList, toList, from, to) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      {list.map(list => <List key={list.title} data={list} />)}
-    </Container>
+    <Boardcontext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
+      </Container>
+    </Boardcontext.Provider>
   );
 }
